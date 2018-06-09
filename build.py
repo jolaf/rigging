bf0c49271eadfb51d25ffb78dad5d79985af759b
Vasily Zakharov <v.zakharov@wwpass.com>
2018-06-09 22:42:28 +0300

    build.py cleanup

--- build.py
+++ build.py
@@ -27,8 +27,72 @@ ZIP_TARGET = 'shtandart.zip'
 BASE64 = 'BASE64'
 NO_INDENT = ''
 
+SCOUR_OPTIONS = {
+    'verbose': True,
+    'digits': 5,
+    'renderer_workaround': True,
+    'strip_xml_prolog': True,
+    'remove_titles': True,
+    'remove_descriptions': True,
+    'remove_metadata': True,
+    'strip_comments': True,
+    'embed_rasters': False,
+    'enable_viewboxing': True,
+    'indent_type': 'space',
+    'indent_depth': 2,
+    'strip_xml_space_attribute': True,
+    'strip_ids': True,
+    'protect_ids_noninkscape': True,
+    'error_on_flowtext': True
+}
+
+SVG_PATTERNS = (
+    (r'(?ms)\s*<metadata>.*?</metadata>(\n*)',
+        r'\1'),
+    (r'(?ms)\s*<g></g>(\n*)',
+        r'\1'),
+    (r' (fill-rule|filter|stroke-miterlimit|stroke-width|style)="\S+"',
+        ''),
+    (r' (height|width)="100%"',
+        ''),
+    (r' (x|y)="0"',
+        ''),
+    (r' ((deck|side|rail|mast|isAcross|ignoreDeck)=")',
+        r' rigging:\1'),
+    (r'\n\n',
+        r'\n')
+)
+
+HTML_PATTERNS = (
+    (r'([ \t]*)<link rel="stylesheet" type="(\S+)" href="(\S+)">',
+        lambda match: loadFile(match, r'\1<style type="\2">\n%s\1</style>', 3)),
+    (r'([ \t]*)<script type="(\S+)" src="(\S+)"></script>',
+        lambda match: loadFile(match, r'\1<script type="\2">\n%s\1</script>', 3)),
+    (r'(?s)(];\n)</script>\n[ \t]*<script type="\S+">.*"use strict";\n',
+        r'\1'),
+    (r'([ \t]*)<object type="image/svg\+xml" data="(\S+)".*?></object>\n',
+        lambda match: loadFile(match, r'%s', 2, NO_INDENT)),
+    (r'(?s)([ \t]*)(scheme = \$\(\'#schemeBlock svg\'\);).*?\n\1}',
+        r'\1\2'),
+    (r'type="(\S+)" href="(\S+)"',
+        lambda match: loadFile(match, r'type="\1" href="data:\1;base64,%s"', 2, BASE64)),
+    (r'<img ([^<>]*) src="((\S+)\.(\S+))"',
+        lambda match: loadImage(match, r'<img \1 src="%s"', 2)),
+    (r' url\("((\S+)\.(\S+))"\)',
+        lambda match: loadImage(match, r' url("%s")')),
+    (r'(\sid="build">)\S+?(</)',
+        lambda match: match.expand(r'\1%s\2' % datetime.utcnow().strftime('b%Y%m%d-%H%MG')))
+)
+
 def getFileName(fileName):
     return join(DIRNAME, fileName)
+
+def scourSVG():
+    options = scourSanitizeOptions()
+    for (option, value) in chain(SCOUR_OPTIONS.items(), {'infilename': getFileName(SVG_SOURCE), 'outfilename': getFileName(SVG_OPTIMIZED)}.items()):
+        setattr(options, option, value)
+    (inputFile, outputFile) = scourGetInOut(scourSanitizeOptions(options))
+    scourStart(options, inputFile, outputFile)
 
 def loadFile(match, replacePattern, fileNamePos, mode = None):
     print(match.groups())
@@ -54,49 +118,6 @@ def loadImage(match, pattern, fileNamePos = 1):
     print(match.groups())
     return match.expand(pattern % ('data:image/%s;base64,%s' % (match.group(fileNamePos + 2).lower(), b64encode(open(getFileName(match.group(fileNamePos)), 'rb').read()).decode())))
 
-SVG_PATTERNS = (
-    (r'(?ms)\s*<metadata>.*?</metadata>(\n*)',
-        r'\1'),
-    (r'(?ms)\s*<g></g>(\n*)',
-        r'\1'),
-    (r' (fill-rule|filter|stroke-miterlimit|stroke-width|style)="\S+"',
-        ''),
-    (r' (height|width)="100%"',
-        ''),
-    (r' (x|y)="0"',
-        ''),
-    (r' ((deck|side|rail|mast|isAcross|ignoreDeck)=")',
-        r' rigging:\1'),
-    (r'\n\n',
-        r'\n')
-)
-
-SCOUR_OPTIONS = {
-    'verbose': True,
-    'digits': 5,
-    'renderer_workaround': True,
-    'strip_xml_prolog': True,
-    'remove_titles': True,
-    'remove_descriptions': True,
-    'remove_metadata': True,
-    'strip_comments': True,
-    'embed_rasters': False,
-    'enable_viewboxing': True,
-    'indent_type': 'space',
-    'indent_depth': 2,
-    'strip_xml_space_attribute': True,
-    'strip_ids': True,
-    'protect_ids_noninkscape': True,
-    'error_on_flowtext': True
-}
-
-def scourSVG():
-    options = scourSanitizeOptions()
-    for (option, value) in chain(SCOUR_OPTIONS.items(), {'infilename': getFileName(SVG_SOURCE), 'outfilename': getFileName(SVG_OPTIMIZED)}.items()):
-        setattr(options, option, value)
-    (inputFile, outputFile) = scourGetInOut(scourSanitizeOptions(options))
-    scourStart(options, inputFile, outputFile)
-
 def cleanSVG():
     with open(getFileName(SVG_OPTIMIZED), 'r') as f:
         data = f.read()
@@ -104,27 +125,6 @@ def cleanSVG():
         data = sub(pattern, replace, data)
     with open(getFileName(SVG_TARGET), 'wb') as f:
         f.write(data.encode())
-
-HTML_PATTERNS = (
-    (r'([ \t]*)<link rel="stylesheet" type="(\S+)" href="(\S+)">',
-        lambda match: loadFile(match, r'\1<style type="\2">\n%s\1</style>', 3)),
-    (r'([ \t]*)<script type="(\S+)" src="(\S+)"></script>',
-        lambda match: loadFile(match, r'\1<script type="\2">\n%s\1</script>', 3)),
-    (r'(?s)(];\n)</script>\n[ \t]*<script type="\S+">.*"use strict";\n',
-        r'\1'),
-    (r'([ \t]*)<object type="image/svg\+xml" data="(\S+)".*?></object>\n',
-        lambda match: loadFile(match, r'%s', 2, NO_INDENT)),
-    (r'(?s)([ \t]*)(scheme = \$\(\'#schemeBlock svg\'\);).*?\n\1}',
-        r'\1\2'),
-    (r'type="(\S+)" href="(\S+)"',
-        lambda match: loadFile(match, r'type="\1" href="data:\1;base64,%s"', 2, BASE64)),
-    (r'<img ([^<>]*) src="((\S+)\.(\S+))"',
-        lambda match: loadImage(match, r'<img \1 src="%s"', 2)),
-    (r' url\("((\S+)\.(\S+))"\)',
-        lambda match: loadImage(match, r' url("%s")')),
-    (r'(\sid="build">)\S+?(</)',
-        lambda match: match.expand(r'\1%s\2' % datetime.utcnow().strftime('b%Y%m%d-%H%MG')))
-)
 
 def compileHTML():
     with open(getFileName(HTML_SOURCE), 'rb') as f:
