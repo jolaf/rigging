46fae2f661adf439f71af1d15ec1e44475d263ec
Vasily Zakharov <v.zakharov@wwpass.com>
2018-06-09 21:40:05 +0300

    processSVG.py integrated into build.py

--- build.py
+++ build.py
@@ -10,8 +10,11 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 DIRNAME = dirname(argv[0])
 
-SOURCE = 'rigging.html'
-TARGET = 'shtandart.html'
+SVG_SOURCE = join('images', 'src', 'shtandart-opt.svg')
+SVG_TARGET = join('images', 'shtandart.svg')
+
+HTML_SOURCE = 'rigging.html'
+HTML_TARGET = 'shtandart.html'
 ZIP_TARGET = 'shtandart.zip'
 
 BASE64 = 'BASE64'
@@ -44,35 +47,65 @@ def loadImage(match, pattern, fileNamePos = 1):
     print(match.groups())
     return match.expand(pattern % ('data:image/%s;base64,%s' % (match.group(fileNamePos + 2).lower(), b64encode(open(getFileName(match.group(fileNamePos)), 'rb').read()).decode())))
 
-PATTERNS = ((r'([ \t]*)<link rel="stylesheet" type="(\S+)" href="(\S+)">',
-                lambda match: loadFile(match, r'\1<style type="\2">\n%s\1</style>', 3)),
-            (r'([ \t]*)<script type="(\S+)" src="(\S+)"></script>',
-                lambda match: loadFile(match, r'\1<script type="\2">\n%s\1</script>', 3)),
-            (r'(?s)(];\n)</script>\n[ \t]*<script type="\S+">.*"use strict";\n',
-                r'\1'),
-            (r'([ \t]*)<object type="image/svg\+xml" data="(\S+)".*?></object>\n',
-                lambda match: loadFile(match, r'%s', 2, NO_INDENT)),
-            (r'(?s)([ \t]*)(scheme = \$\(\'#schemeBlock svg\'\);).*?\n\1}', r'\1\2'),
-            (r'type="(\S+)" href="(\S+)"',
-                lambda match: loadFile(match, r'type="\1" href="data:\1;base64,%s"', 2, BASE64)),
-            (r'<img ([^<>]*) src="((\S+)\.(\S+))"',
-                lambda match: loadImage(match, r'<img \1 src="%s"', 2)),
-            (r' url\("((\S+)\.(\S+))"\)',
-                lambda match: loadImage(match, r' url("%s")')),
-            (r'(\sid="build">)\S+?(</)',
-                lambda match: match.expand(r'\1%s\2' % datetime.utcnow().strftime('b%Y%m%d-%H%MG'))))
+SVG_PATTERNS = (
+    (r'(?ms)\s*<metadata>.*?</metadata>(\n*)',
+        r'\1'),
+    (r'(?ms)\s*<g></g>(\n*)',
+        r'\1'),
+    (r' (fill-rule|filter|stroke-miterlimit|stroke-width|style)="\S+"',
+        ''),
+    (r' (height|width)="100%"',
+        ''),
+    (r' (x|y)="0"',
+        ''),
+    (r' ((deck|side|rail|mast|isAcross|ignoreDeck)=")',
+        r' rigging:\1'),
+    (r'\n\n',
+        r'\n'))
+
+def compileSVG():
+    with open(getFileName(SVG_SOURCE), 'r') as f:
+        data = f.read()
+    for (pattern, replace) in SVG_PATTERNS:
+        data = sub(pattern, replace, data)
+    with open(getFileName(SVG_TARGET), 'wb') as f:
+        f.write(data.encode())
+
+HTML_PATTERNS = (
+    (r'([ \t]*)<link rel="stylesheet" type="(\S+)" href="(\S+)">',
+        lambda match: loadFile(match, r'\1<style type="\2">\n%s\1</style>', 3)),
+    (r'([ \t]*)<script type="(\S+)" src="(\S+)"></script>',
+        lambda match: loadFile(match, r'\1<script type="\2">\n%s\1</script>', 3)),
+    (r'(?s)(];\n)</script>\n[ \t]*<script type="\S+">.*"use strict";\n',
+        r'\1'),
+    (r'([ \t]*)<object type="image/svg\+xml" data="(\S+)".*?></object>\n',
+        lambda match: loadFile(match, r'%s', 2, NO_INDENT)),
+    (r'(?s)([ \t]*)(scheme = \$\(\'#schemeBlock svg\'\);).*?\n\1}',
+        r'\1\2'),
+    (r'type="(\S+)" href="(\S+)"',
+        lambda match: loadFile(match, r'type="\1" href="data:\1;base64,%s"', 2, BASE64)),
+    (r'<img ([^<>]*) src="((\S+)\.(\S+))"',
+        lambda match: loadImage(match, r'<img \1 src="%s"', 2)),
+    (r' url\("((\S+)\.(\S+))"\)',
+        lambda match: loadImage(match, r' url("%s")')),
+    (r'(\sid="build">)\S+?(</)',
+        lambda match: match.expand(r'\1%s\2' % datetime.utcnow().strftime('b%Y%m%d-%H%MG'))))
 
 def compileHTML():
-    with open(getFileName(SOURCE), 'rb') as f:
+    with open(getFileName(HTML_SOURCE), 'rb') as f:
         data = f.read().decode()
-    for (pattern, replace) in PATTERNS:
+    for (pattern, replace) in HTML_PATTERNS:
         data = sub(pattern, replace, data)
-    with open(getFileName(TARGET), 'wb') as f:
+    with open(getFileName(HTML_TARGET), 'wb') as f:
         f.write(data.encode())
     with ZipFile(getFileName(ZIP_TARGET), 'w', ZIP_DEFLATED) as f:
-        f.write(getFileName(TARGET))
+        f.write(getFileName(HTML_TARGET))
 
 def main():
+    print("Compiling SVG...")
+    compileSVG()
+    print("Compiling HTML...")
     compileHTML()
+    print("DONE")
 
 main()
